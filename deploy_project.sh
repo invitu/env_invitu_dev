ee15b74d24b48965f91841e866b65bb619504d49
Cyril VINH-TUNG <cyril@invitu.com>
2024-05-08 10:32:08 -1000

    [IMP] Dispatch modules in special or invitu

--- deploy_project.sh
+++ deploy_project.sh
@@ -32,6 +32,7 @@ fi
 echo "On importe les modules contenus dans modules.csv"
 
 
+TTY=`tty`
 #Read each line of the file in each iteration
 
 while IFS="," read -r module_name module_link
@@ -49,18 +50,42 @@ elif [[ $module_link == http* ]]; then
     echo 'Module via url : on fait un tar.gz direct'
     cd $projectpath
     mv $module_name "$module_name"_"$(date +%Y%m%d)"
-    tar cvzf /tmp/"$module_name"_v"$odooversion"_"$(date +%Y%m%d)".tar.gz "$module_name"_"$(date +%Y%m%d)"
+    #while true
+    #do
+    echo $module_name $module_link
+    read -p "invitu (1) ou special (2) ?" input <$TTY
+    case $input in
+        exit) break ;;
+        1)
+            echo "Invitu"
+            tar cvzf /tmp/"$module_name"_v"$odooversion"_invitu_"$(date +%Y%m%d)".tar.gz "$module_name"_"$(date +%Y%m%d)"
+            echo "scp '$module_name'_v'$odooversion'_invitu_$(date +%Y%m%d).tar.gz"
+            scp -P2222 /tmp/"$module_name"_v"$odooversion"_invitu_"$(date +%Y%m%d)".tar.gz root@odoov$odooversion:.
+            ;;
+        2)
+            echo "Special"
+            tar cvzf /tmp/"$module_name"_v"$odooversion"_special_"$(date +%Y%m%d)".tar.gz "$module_name"_"$(date +%Y%m%d)"
+            echo "scp '$module_name'_v'$odooversion'_special_$(date +%Y%m%d).tar.gz"
+            scp -P2222 /tmp/"$module_name"_v"$odooversion"_special_"$(date +%Y%m%d)".tar.gz root@odoov$odooversion:.
+            ;;
+        *) echo "Unknown response, enter a number 1-2 or type 'exit' to quit" ;;
+    esac
+    #done
     mv "$module_name"_"$(date +%Y%m%d)" $module_name
-    echo "scp '$module_name'_v'$odooversion'_$(date +%Y%m%d).tar.gz"
-    scp -P2222 /tmp/"$module_name"_v"$odooversion"_"$(date +%Y%m%d)".tar.gz root@odoov$odooversion:.
-
 
 elif  [[ $module_link == git* ]]; then
     echo 'Module via repos : on fait un archive'
     cd $projectpath/$module_name
-    git archive --format tgz --prefix "$module_name"_"$(date +%Y%m%d)"/ --output /tmp/"$module_name"_v"$odooversion"_"$(date +%Y%m%d)".tar.gz $odooversion.0
-    echo "scp '$module_name'_v'$odooversion'_$(date +%Y%m%d).tar.gz"
-    scp -P2222 /tmp/"$module_name"_v"$odooversion"_"$(date +%Y%m%d)".tar.gz root@odoov$odooversion:.
+    if [[ $module_link == "gitinvitu"* ]];
+    then
+        git archive --format tgz --prefix "$module_name"_"$(date +%Y%m%d)"/ --output /tmp/"$module_name"_v"$odooversion"_invitu_"$(date +%Y%m%d)".tar.gz $odooversion.0
+        echo "scp '$module_name'_v'$odooversion'_invitu_$(date +%Y%m%d).tar.gz"
+        scp -P2222 /tmp/"$module_name"_v"$odooversion"_invitu_"$(date +%Y%m%d)".tar.gz root@odoov$odooversion:.
+    else
+        git archive --format tgz --prefix "$module_name"_"$(date +%Y%m%d)"/ --output /tmp/"$module_name"_v"$odooversion"_special_"$(date +%Y%m%d)".tar.gz $odooversion.0
+        echo "scp '$module_name'_v'$odooversion'_special_$(date +%Y%m%d).tar.gz"
+        scp -P2222 /tmp/"$module_name"_v"$odooversion"_special_"$(date +%Y%m%d)".tar.gz root@odoov$odooversion:.
+    fi
 
 else
     echo "Erreur dans le fichier de déclaration des modules $module_name $module_link"
